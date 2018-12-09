1e3a3fe5d4d8947f3a755c7d9326cff7d01cc679
your3i <your3i@yahoo.co.jp>
2018-12-09 14:00:21 +0900

    First Commit

--- ScrollMultipleScrollViews/ViewController.swift
+++ ScrollMultipleScrollViews/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outerScrollView: UIScrollView!
+    @IBOutlet weak var bottomScrollView: UIScrollView!
+    @IBOutlet weak var topScrollView: UIScrollView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
+        // Aim to scroll the top scrollView backward,
+        // so set the top scrollView scrolled to the right edge at first
+        topScrollView.contentOffset = CGPoint(x: topScrollView.bounds.width, y: 0.0)
+    }
 }
 
+extension ViewController: UIScrollViewDelegate {
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard scrollView == outerScrollView else {
+            return
+        }
+
+        // Calculate scroll progress
+        let outerScrollDistance = outerScrollView.contentSize.width - outerScrollView.bounds.width
+        let progress = scrollView.contentOffset.x / outerScrollDistance
+
+        // Scroll the top scrollView backward programmatically
+        let topScrollDistance = topScrollView.contentSize.width - topScrollView.bounds.width
+        let topOffsetX = (1 - progress) * topScrollDistance
+        topScrollView.contentOffset = CGPoint(x: topOffsetX, y: 0.0)
+
+        // Scroll the bottom scrollView forward programmatically
+        let bottomScrollDistance = bottomScrollView.contentSize.width - bottomScrollView.bounds.width
+        let bottomOffsetX = progress * bottomScrollDistance
+        bottomScrollView.contentOffset = CGPoint(x: bottomOffsetX, y: 0.0)
+    }
+}
